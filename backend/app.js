5678741d912ef44c2b68ff1382c177ca07d3d9b3
Viki Bugai <vik-onegina@yandex.ru>
2023-05-15 19:19:31 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const cors = require('cors');
 const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
 const limiter = require('./middlewares/rateLimit');
+const originCors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const handleError = require('./middlewares/handleError');
 
@@ -24,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(limiter);
-app.use(cors());
+app.use(cors(originCors));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,13 @@
+const originCors = {
+  origin: [
+    'https://domainname.students.nomoredomains.monster',
+    'http://domainname.students.nomoredomains.monster',
+    'https://api.domainname.stud.viki.nomoredomains.monster',
+    'http://api.domainname.stud.viki.nomoredomains.monster',
+    'https://locahost:3000',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+};
+
+module.exports = originCors;
